b09436b00938317a4ce8a45c69e75c8ef45cbf3a
DevMarioNan <dev.mario.nan@gmail.com>
2026-04-21 18:00:54 +0200

    fix: use client component with fetch for Instagram OAuth redirect

--- app/test-db/page.tsx
+++
@@ -1,9 +0,0 @@
-import { db } from "@/lib/db";
-
-export default async function TestDb() {
-  const users = await db.user.findMany();
-  return <div>
-    <h1>Users</h1>
-    <pre>{JSON.stringify(users, null, 2)}</pre>
-  </div>
-}   
